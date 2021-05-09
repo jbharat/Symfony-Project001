89359eea8388097e98583abea18f860e6295f3f9
Bharat <jbharat@obsglobal.com>
2021-05-09 15:39:11 -0500

    Git prcatice

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,10 +1,11 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
 	/**
 	 * @Route("/")
@@ -19,9 +20,15 @@ class QuestionController
 	 */
 	public function show($slug)
 	{
-		return new Response(sprintf(
+		$answers = ['First','Second','Third'];
+		return $this->render('question/show.html.twig',[
+			'question' => ucwords(str_replace('-',' ',$slug)),
+			'answers' => $answers
+		]);
+	
+		/*  return new Response(sprintf(
 			'Future page to show a question "%s"!',
-			ucwords(str_replace('-',' ',$slug))
-		));
+		 	ucwords(str_replace('-',' ',$slug))
+			)); */
 	}
 }
